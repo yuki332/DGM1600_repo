10cbb645978465c2b00541a36fab9bc34ccad27d
Yuki Arai <sk80332@gmail.com>
2015-09-01 09:51:49 -0600

    var and fucntion

---
+++ var and function/Assets/variablesAndFunction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variablesAndFunction : MonoBehaviour
+{   
+	int myInt = 10;
+	
+	
+	void Start ()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	
+	
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}

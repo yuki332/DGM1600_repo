62703f4762d9f1b07a1f4eb34598360eada958f0
unknown <10541180@CS514-23.ad.uvu.edu>
2015-09-29 09:20:31 -0600

    if statement

---
+++ conventionsandSyntax/Assets/conventionsandsyntaxpassoff.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class conventionsandsyntaxpassoff : MonoBehaviour
+{
+	void Start ()
+	{
+		//This is a single line comment.
+		
+		/*This is a multiple line comment or block 
+		of code that needs to be commented out.
+		*/
+	}
+
+	void Update()
+	{
+		//The following line of code is used to track the position on the falling cube. Enter the correct convention.
+		Debug.Log (transform.position.y);
+
+		/*
+		The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10. 
+		Enter the correct statement.
+		*/
+
+		if (transform.position.y <= 10) 
+		{
+			Debug.Log ("I'm going to splater!");
+		}
+	}
+}

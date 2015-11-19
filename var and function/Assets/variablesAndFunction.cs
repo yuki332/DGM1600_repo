c04a2f5b45125c12ff14494f962c204ab94c02c6
Yuki Arai <sk80332@gmail.com>
2015-11-19 01:24:07 -0700

    first commit

---
+++ var and function/Assets/variablesAndFunction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variablesAndFunction : MonoBehaviour
+{   
+	int myInt = 2;
+	
+	
+	void Start ()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	
+	
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 4;
+		return ret;
+	}
+}
